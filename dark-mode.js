c9b39d2f14cd55bf4eaf49c33038682c68914a1c
nazu280 <66703146+nazu280@users.noreply.github.com>
2021-11-20 09:43:49 +0900

    Update dark-mode.js

--- dark-mode.js
+++ dark-mode.js
@@ -1,7 +1,50 @@
-if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    document.documentElement.classList.add("dark")
-}
+var btn = $('.toggleTheme');
 
-document.getElementById("toggleTheme").addEventListener("click",() => {
-  document.documentElement.classList.toggle("dark")
-})
+
+var width = 0;
+var height = 0;
+var r = 0;
+setSize();
+
+function setSize() {
+  width = $(window).width();
+  height = $(window).height();
+  r = Math.sqrt(width * width + height * height);
+}
+$(window).resize(setSize);
+
+btn.click(function(e) {
+  btn.removeClass('current');
+  $(this).addClass('current');
+  var circle = $("<div unselectable='on' id='circle'></div>");
+  background.append(circle);
+  circle.css({
+    position: 'absolute',
+    'background-color': $(this).css('background-color'),
+    width: 0,
+    height: 0,
+    "border-radius": "50%",
+    left: e.pageX,
+    top: e.pageY,
+    'margin-left': 0,
+    'margin-top': 0,
+    'webkit-user-select': 'none',
+    '-moz-user-select': 'none',
+    '-ms-user-select': 'none'
+  });
+  circle.animate({
+    width: (r * 2),
+    height: (r * 2),
+    'margin-left': -r,
+    'margin-top': -r
+  }, {
+    duration: 600,
+    easing: "easeInOutCubic",
+    queue: false,
+    complete: function() {
+     circle.parent().css('background-color',
+                    $(this).css('background-color'));
+     circle.detach();
+    }
+  });
+});
