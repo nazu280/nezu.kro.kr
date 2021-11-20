eed13eca9dd6744900454d6e626329891be8c94e
nazu280 <66703146+nazu280@users.noreply.github.com>
2021-11-20 16:45:32 +0900

    Update nezu.js

--- nezu.js
+++ nezu.js
@@ -14,6 +14,27 @@ document.querySelector(".container").addEventListener("click", () => {
     document.querySelector("body").classList.toggle("dark");
 })
 
+var Target = document.getElementById("clock");
+var Target_apm = document.getElementById("apm");
+function clock() {
+    var time = new Date();
+    var hours = time.getHours();
+    var minutes = time.getMinutes();
+    var seconds = time.getSeconds();
+    var AmPm ="AM";
+    if(hours > 12){   
+        var AmPm ="PM";
+        hours %= 12;
+    }
+
+    Target.innerText = 
+    `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+
+    Target_apm.innerText = `${AmPm}`;
+
+}
+clock();
+setInterval(clock, 1000);
 
 const se = document.createElement('script');
 se.src = 'https://ipinfo.io?callback=callback';
@@ -22,12 +43,4 @@ document.body.removeChild(se);
 
 function callback(data) {
     document.getElementById('client-ip').textContent = data.ip;
-}
-
-function copyToClipBoard() {
-
-    var content = document.getElementById('client-ip');
-    
-    content.select();
-    document.execCommand('copy');
 }
