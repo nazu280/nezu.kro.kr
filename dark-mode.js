e64046ad53c44471c44971b6c3347e89acbde960
nazu280 <66703146+nazu280@users.noreply.github.com>
2021-11-20 09:46:00 +0900

    Update dark-mode.js

--- dark-mode.js
+++ dark-mode.js
@@ -1,3 +1,14 @@
+/*
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    document.documentElement.classList.add("dark")
+}
+
+document.getElementById("toggleTheme").addEventListener("click",() => {
+  document.documentElement.classList.toggle("dark")
+})
+
+*/
+
 var btn = $('.toggleTheme');
 
 
