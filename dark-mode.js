fa43d51773ec814846ac58f02d724d6e66440624
nazu280 <66703146+nazu280@users.noreply.github.com>
2021-11-20 14:18:53 +0900

    Update dark-mode.js

--- dark-mode.js
+++ dark-mode.js
@@ -1,8 +1,14 @@
-if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+/* if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
     document.documentElement.classList.add("dark")
 }
 
 document.getElementById("toggleTheme").addEventListener("click",() => {
   document.documentElement.classList.toggle("dark")
+}) */
+
+document.querySelector(".container").addEventListener("click", () => {
+    document.querySelector(".sun-logo").classList.toggle("animate-sun");
+    document.querySelector(".moon-logo").classList.toggle("animate-moon");
+    document.querySelector("body").classList.toggle("dark");
 })
 
