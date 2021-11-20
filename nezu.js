9a4a3a1ffd8b7623fc4b9e67b8e4d7cecf7d0212
nazu280 <66703146+nazu280@users.noreply.github.com>
2021-11-20 18:12:42 +0900

    Update nezu.js

--- nezu.js
+++ nezu.js
@@ -51,39 +51,3 @@ function copyip() {
     var ip = document.querySelector('.copy-ip');
     navigator.clipboard.writeText(ip.textContent)
 }
-
-function fillWidth(elem, timer, limit) {
-	if (!timer) { timer = 3000; }	
-	if (!limit) { limit = 100; }
-	var width = 1;
-	var id = setInterval(frame, timer / 100);
-		function frame() {
-		if (width >= limit) {
-			clearInterval(id);
-		} else {
-			width++;
-			elem.style.width = width + '%';
-		}
-	}
-};
-
-function toast(msg, timer) {
-	if (!timer) { timer = 3000; }
-	var $elem = $("<div class='toastWrap'><span class='toast'>" + msg + "<b></b><div class='timerWrap'><div class='timer'></div></div></span></div>");
-	$("#toast").append($elem); //top = prepend, bottom = append
-	$elem.slideToggle(100, function() {
-		$('.timerWrap', this).first().outerWidth($elem.find('.toast').first().outerWidth() - 10);
-		if (!isNaN(timer)) {
-			fillWidth($elem.find('.timer').first()[0], timer);
-			setTimeout(function() {
-				$elem.fadeOut(function() {
-					$(this).remove();
-				});
-			}, timer);			
-		}
-	});
-}
-
-$("#toast").on("click", "b", function() {
-	$(this).closest('.toastWrap').remove();
-})
