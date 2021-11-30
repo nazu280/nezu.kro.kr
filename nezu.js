7187d64807a753409f06d54eef60bd7cdb8ee856
nazu280 <66703146+nazu280@users.noreply.github.com>
2021-11-30 21:22:35 +0900

    Update nezu.js

--- nezu.js
+++ nezu.js
@@ -1,19 +1,3 @@
-if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    document.querySelector(".sun-logo").classList.toggle("animate-sun");
-    document.querySelector(".moon-logo").classList.toggle("animate-moon");
-    document.querySelector("body").classList.toggle("dark");
-}
-
-/*document.getElementById("toggleTheme").addEventListener("click",() => {
-  document.documentElement.classList.toggle("dark")
-}) */
-
-document.querySelector(".container").addEventListener("click", () => {
-    document.querySelector(".sun-logo").classList.toggle("animate-sun");
-    document.querySelector(".moon-logo").classList.toggle("animate-moon");
-    document.querySelector("body").classList.toggle("dark");
-})
-
 var Target = document.getElementById("clock");
 var Target_apm = document.getElementById("apm");
 function clock() {
